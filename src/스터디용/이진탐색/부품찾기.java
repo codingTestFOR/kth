48911a1470225fdac7a24b04f56e37934cfa64dc
kth <kth1017@github.com>
2025-05-12 15:31:25 +0900

    정렬 부품 찾기 추가 풀이

--- src/스터디용/이진탐색/부품찾기.java
+++ src/스터디용/이진탐색/부품찾기.java
@@ -1,9 +1,7 @@
 package 스터디용.이진탐색;
 
 import java.io.*;
-import java.util.Arrays;
-import java.util.Scanner;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class 부품찾기 {
     public static void main(String[] args) throws IOException {
@@ -28,34 +26,26 @@ public class 부품찾기 {
 
         System.out.println("m 부품 번호 입력 : ");
         int[] arrM = new int[m+1];
-        for (int i = 0; i < m; i++) {
+        for (int i = 1; i < m+1; i++) {
             arrM[i] = sc.nextInt();
         }
 
-        // arrN 출력
-        System.out.print("정렬된 n 부품 배열: ");
-        for (int i = 0; i < n; i++) {
-            System.out.print(arrN[i] + " ");
-        }
-        System.out.println(); // 줄바꿈
-
-        // arrM 출력
-        System.out.print("입력된 m 부품 배열: ");
-        for (int i = 0; i < m; i++) {
-            System.out.print(arrM[i] + " ");
-        }
-        System.out.println();
-
-        for (int i : arrM) {
+        for (int i = 1; i < m+1; i++) {
             System.out.println("재귀 함수 이용시 : " +
                     (recursive(arrN, i, 0, n - 1) > -1 ? "yes" : "no"));
+            System.out.println("-------------");
             System.out.println("반복문 이용시 : " +
                     (loop(arrN, i, 0, n - 1) > -1 ? "yes" : "no"));
+            System.out.println("-------------");
         }
-    }
 
+        countingSort(arrN, arrM);
+        System.out.println("-------------");
+        set(arrN, arrM);
+    }
     // 재귀함수
     public static int recursive(int[] arr, int target, int start, int end){
+        // 탈출 조건
         if (start > end) {
             return -1;
         }
@@ -85,5 +75,39 @@ public class 부품찾기 {
         }
 
         return -1;
+    }
+
+    //계수정렬
+    public static void countingSort(int[] arrN, int[] arrM){
+        int[] table = new int[arrN[arrN.length - 1] + 1];
+        for (int i : arrN) {
+            table[i] = 1;
+        }
+
+        for (int i = 1; i < arrM.length; i++ ) {
+            if (table[i] == 1) {
+                System.out.println("yes");
+            } else {
+                System.out.println("no");
+            }
+        }
+    }
+
+
+    //집합
+    public static void set(int[] arrN, int[] arrM){
+         Set<Integer> set = new HashSet<>();
+         for (int num : arrN) {
+             set.add(num);
+         }
+
+        for (int i = 1; i < arrM.length; i++ ) {
+            if (set.contains(arrM[i])) {
+                System.out.println("yes");
+            } else {
+                System.out.println("no");
+            }
+        }
+
     }
 }
